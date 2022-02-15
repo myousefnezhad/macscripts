6a70c09aa0a95962b2a02fc0fe0fad1557f747ce
Tony Yousefnezhad <tony@flash.local>
2022-02-14 20:49:24 -0700

    add mpv and swig

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -1,1 +1,1 @@
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig
