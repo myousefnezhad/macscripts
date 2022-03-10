c402f36f7d55d6b6b7d6be15eb5cefd09a81cd97
Tony Yousefnezhad <myousefnezhad@gmail.com>
2022-03-09 23:42:21 -0700

    update brew, home soft link, and ip of nfs

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -1,3 +1,3 @@
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg
 
 brew install --cask qt-creator
