5e17f75e106bbecc2e8c5380daecff7b550a7120
Tony Yousefnezhad <tony@iTonyMax.local>
2022-03-26 05:39:50 -0600

    new vim setting

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -1,3 +1,3 @@
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg vim
 
 brew install --cask qt-creator
