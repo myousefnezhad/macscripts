78b95e1193b1502e8dcfd3a00db7a58e74bbe39e
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-01-30 09:38:09 -0700

    pinta is added

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -1,3 +1,3 @@
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg vim
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg vim pinta
 
 brew install --cask qt-creator
