81f3db223e73f4b1eddb75b591bf30a57e134600
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-04-26 16:47:47 -0600

    Add new app for kube

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -1,4 +1,9 @@
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg vim pinta jq iperf helm md5sha1sum kubectx
+brew update
+
+brew upgrade 
+
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg vim pinta jq iperf helm md5sha1sum ku
+bectx kubernetes-cli
 
 brew install --cask qt-creator
 
