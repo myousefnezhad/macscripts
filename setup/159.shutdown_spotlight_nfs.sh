6fa1fef208c00b5971f4378319dc12b2c64f63d6
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-23 04:16:32 -0600

    dot underscore issue

---
+++ setup/159.shutdown_spotlight_nfs.sh
@@ -0,0 +1,14 @@
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+sudo mdutil -i off -d /IRONMAN
+sudo mdutil -i off -d /IRONMAN1
+sudo mdutil -i off -d /IRONMAN2
+sudo mdutil -i off -d /IRONMAN3
+sudo mdutil -i off -d /IRONMAN4
+sudo mdutil -i off -d /IRONMAN5
+sudo mdutil -i off -d /IRONMAN6
+sudo mdutil -i off -d /IRONMAN20
+sudo mdutil -i off -d /IRONMAN21
+sudo mdutil -i off -d /IRONMAN22
+sudo mdutil -i off -d /IRONMAN30
+sudo mdutil -i off -d /THOR1
+sudo mdutil -i off -d /THOR2
