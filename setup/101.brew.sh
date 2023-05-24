3ca9b2a5e93a118dd57be61bdc3be782c1367cce
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-24 06:36:28 -0600

    new vim app runner

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -2,9 +2,9 @@ brew update
 
 brew upgrade 
 
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg pinta jq iperf helm md5sha1sum ku rclone
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg pinta jq iperf helm md5sha1sum ku rclone vim
 
-brew install --cask macvim
+#brew install --cask macvim
 
 bectx kubernetes-cli
 
--- setup/154.vim.sh
+++ setup/154.vim.sh
@@ -1,3 +1,3 @@
 ln -fs $HOME/opt/scripts/settings/vim/vimrc $HOME/.vimrc
-ln -sf $HOME/opt/scripts/settings/vim/gvimrc $HOME/.gvimrc
+#ln -sf $HOME/opt/scripts/settings/vim/gvimrc $HOME/.gvimrc
 ln -fs $HOME/opt/vim/ $HOME/.vim
