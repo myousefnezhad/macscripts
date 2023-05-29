91c6eadb41cd14984e3fe540a65992803c94a682
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-05-29 00:37:04 -0600

    neovim instead of vim

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -2,9 +2,7 @@ brew update
 
 brew upgrade 
 
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg pinta jq iperf helm md5sha1sum ku rclone vim
-
-#brew install --cask macvim
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg pinta jq iperf helm md5sha1sum ku rclone neovim
 
 bectx kubernetes-cli
 
--- setup/155.neovim.sh
+++ setup/155.neovim.sh
@@ -1,3 +1,14 @@
-mkdir -p $HOME/.config/nvim/bundle
-ln -fs $HOME/opt/neovim/ $HOME/.config/nvim/bundle/Vundle.vim
-ln -fs $HOME/opt/scripts/settings/neovim/init.vim $HOME/.config/nvim/init.vim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\\n ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# Python Debuger
+mkdir ~/.virtualenvs
+cd ~/.virtualenvs
+python -m venv debugpy
+debugpy/bin/python -m pip install debugpy
+# Javascript Debugger
+git clone https://github.com/microsoft/vscode-js-debug ~/.vscode-js-debug
+cd ~/.vscode-js-debug
+npm install --legacy-peer-deps
+npx gulp vsDebugServerBundle
+mv dist out
+
+ln -sf $HOME/opt/scripts/settings/nvim $HOME/.config/nvim
