f6dbd2e3d9362f3664bfd84b75de10b92d503c3c
Tony Yousefnezhad <myousefnezhad@gmail.com>
2023-09-11 04:17:36 -0600

    New Jerico app, icon PNGs; Neovim HTML autotag, CSS formatter and LS

--- settings/nvim/lua/user/plugins/mason.lua
+++ settings/nvim/lua/user/plugins/mason.lua
@@ -8,7 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "lua_ls", "tsserver", "html", "jsonls", "ltex", "remark_ls",
-        "yamlls", "rust_analyzer", "sqls", "pylsp", "gopls"
+        "yamlls", "rust_analyzer", "sqls", "pylsp", "gopls", "cssls",
       })
     end,
   },
@@ -20,6 +20,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "prettier",
+        "prettierd",
         "pylint",
         "sql-formatter",
         -- "stylua",
