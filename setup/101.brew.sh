f0246317818ebf38efee42bbdf09de7abccf5d2b
Tony Yousefnezhad <myousefnezhad@gmail.com>
2025-09-14 14:49:45 -0600

    fpga

--- setup/101.brew.sh
+++ setup/101.brew.sh
@@ -2,7 +2,7 @@ brew update
 
 brew upgrade 
 
-brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg pinta jq iperf helm md5sha1sum rclone neovim lsd asitop scala zig jdtls kotlin-language-server maven gradle surfer icarus-verilog
+brew install mc neofetch tmux jcal tree ed htop glances convertlit ffmpeg nmap xclip hexedit youtube-dl wget mpv swig gitg pinta jq iperf helm md5sha1sum rclone neovim lsd asitop scala zig jdtls kotlin-language-server maven gradle surfer icarus-verilog ghdl
 
 bectx kubernetes-cli
 
